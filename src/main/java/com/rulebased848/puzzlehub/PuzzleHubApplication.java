3e027140f6dfddae324739d1a7f8506e48873fce
ruleBased848 <rkdfowns@naver.com>
2023-01-05 18:40:17 +0900

    feat: add the main class

---
+++ src/main/java/com/rulebased848/puzzlehub/PuzzleHubApplication.java
@@ -0,0 +1,11 @@
+package com.rulebased848.puzzlehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzleHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PuzzleHubApplication.class, args);
+    }
+}
